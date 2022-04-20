49f70ab13ad0634e2d31abf2bdb38d23379afc18
nicoleyimessier <nicoleyimessier@gmail.com>
2022-04-19 21:54:15 -0400

    setup local nodejs with updated transcribe file

--- scripts/transcribe.bat
+++ scripts/transcribe.bat
@@ -2,9 +2,10 @@
 
 set GOOGLE_APPLICATION_CREDENTIALS=%1
 
-cd C:\Users\nicol\Documents\creative\projects\ancient_futures\code\nodejs-speech
+::cd C:\Users\nicol\Documents\creative\projects\ancient_futures\code\nodejs-speech
+cd C:\Users\nicol\Documents\creative\projects\ancient_futures\code\AncientFutures\nodejs-speech
 
-node samples\recognize.js sync %2
+node recognize.js sync %2
 
 :done
 echo.
