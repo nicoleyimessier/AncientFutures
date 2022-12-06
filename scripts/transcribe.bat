e713e7dcd56c295173d3f9df6f8b8657af880786
nicoleyimessier <nicoleyimessier@gmail.com>
2022-12-05 19:02:28 -0500

    new animations

--- scripts/transcribe.bat
+++ scripts/transcribe.bat
@@ -3,8 +3,8 @@
 set GOOGLE_APPLICATION_CREDENTIALS=%1
 
 ::cd C:\Users\nicol\Documents\creative\projects\ancient_futures\code\nodejs-speech
-::cd C:\Users\nicol\Documents\creative\projects\ancient_futures\code\AncientFutures\nodejs-speech
-cd C:\ancient_futures\AncientFutures\nodejs-speech
+cd C:\Users\nicol\Documents\creative\projects\ancient_futures\code\AncientFutures\nodejs-speech
+::cd C:\ancient_futures\AncientFutures\nodejs-speech
 
 node recognize.js sync %2
 
