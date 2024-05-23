58b95596a8351b7be6745af5f0f08a983b8196fa
nicoleyimessier <nicoleyimessier@gmail.com>
2024-05-23 18:18:34 -0400

    start up script

--- scripts/startup.bat
+++ scripts/startup.bat
@@ -1,9 +1,12 @@
 ::netsh advfirewall set allprofiles state off
 
-::taskkill /f /im explorer.exe /fi "memusage gt 2"
+taskkill /f /im explorer.exe /fi "memusage gt 2"
 ::taskkill /f /im node.exe
 
-::timeout /t 10 
+timeout /t 3 
+
+start cmd.exe /K "cd c:\ancient_futures\AncientFutures\stand_alone_version && node index.js"
+
 start cmd.exe /K "cd c:\ancient_futures\of_v0.11.2_vs2017_release\apps\myApps\AncientFuturesOF\bin && AncientFuturesOF.exe"
 
 ::npm run ampm
